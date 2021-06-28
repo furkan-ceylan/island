f1aa682fefd2cce87486441bff939d01669126f0
furkan ceylan <ceylan.furkan@outlook.com>
2021-06-29 02:49:43 +0300

    Add bcrypt lib and test post data to db

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,22 @@
 const router = require('express').Router()
+const UserModal = require('../models/User.js')
+const bcrypt = require('bcrypt')
 
-router.get('/', (req, res) => {
-  res.send('auth route')
+router.post('/register', async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10)
+    const hashedPass = await bcrypt.hash(req.body.password, salt)
+
+    const newUser = new UserModal({
+      email: req.body.email,
+      password: hashedPass,
+    })
+
+    const user = await newUser.save()
+    res.status(200).json(user)
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 module.exports = router
