aa593bd787fc224421019393c5ad545043e84ba9
furkan ceylan <ceylan.furkan@outlook.com>
2021-06-30 01:06:05 +0300

    Add login router

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,14 @@
 const router = require('express').Router()
-const UserModal = require('../models/User.js')
+const User = require('../models/User.js')
 const bcrypt = require('bcrypt')
 
+//REGISTER
 router.post('/register', async (req, res) => {
   try {
     const salt = await bcrypt.genSalt(10)
     const hashedPass = await bcrypt.hash(req.body.password, salt)
 
-    const newUser = new UserModal({
+    const newUser = new User({
       email: req.body.email,
       password: hashedPass,
     })
@@ -15,7 +16,27 @@ router.post('/register', async (req, res) => {
     const user = await newUser.save()
     res.status(200).json(user)
   } catch (err) {
-    console.log(err)
+    res.status(500).json(err)
+  }
+})
+
+//LOGIN
+router.post('/login', async (req, res) => {
+  try {
+    const userLogin = await User.findOne({
+      email: req.body.email,
+    })
+    !userLogin && res.status(404).json('User not found')
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      userLogin.password
+    )
+    !validPassword && res.status(400).json('Wrong password')
+
+    res.status(200).json(userLogin)
+  } catch (err) {
+    res.status(500).json(err)
   }
 })
 
