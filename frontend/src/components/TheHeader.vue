100b5143db46b42815e0b8585299d84fc980820f
furkan ceylan <ceylan.furkan@outlook.com>
2021-07-01 00:55:55 +0300

    Add root colors

---
+++ frontend/src/components/TheHeader.vue
@@ -0,0 +1,121 @@
+<template>
+  <header>
+    <div class="header">
+      <div class="header__left">
+        <img class="header__logo" src="../assets/logo.png" />
+        <i class="material-icons">home</i>
+      </div>
+      <div class="header__main">
+        <h2 class="header__main-text">Home</h2>
+        <div class="header__main-right">
+          <div class="header__main-right-search">
+            <input type="text" placeholder="Search" />
+            <i class="material-icons">search</i>
+          </div>
+          <button class="btn">Add a Post</button>
+        </div>
+      </div>
+      <div class="header__user">
+        <label class="header__user-username">username</label>
+        <img
+          class="header__user-image"
+          src="https://png.clipart.me/istock/previews/7063/70633839-person-avatar.jpg"
+        />
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.header {
+  height: 10vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.5rem 1.5rem;
+}
+
+.header__left {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 15%;
+}
+
+.header__logo {
+  width: 120px;
+}
+
+.header__left i {
+  margin-left: auto;
+  right: 0;
+  border-bottom: 2px solid var(--pink);
+}
+
+.header__main {
+  width: 60%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.header__main-text {
+  font-size: 1.3rem;
+  font-weight: 700;
+}
+
+.header__main-right {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.header__main-right-search {
+  background-color: white;
+  margin-right: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.header__main-right input {
+  height: 30px;
+  border-radius: 5px;
+  padding-left: 5px;
+}
+
+.header__main-right i {
+  height: 30px;
+  border-radius: 7px;
+}
+
+.header__main-right button {
+  width: 120px;
+  font-weight: 600;
+  font-size: 0.75em;
+  margin-top: 0.7rem;
+}
+
+.header__user {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  width: 20%;
+}
+
+.header__user-username {
+  font-weight: 500;
+  font-size: 0.9em;
+  margin-right: 0.5em;
+}
+
+.header__user-image {
+  width: 40px;
+  height: 40px;
+  border-radius: 100%;
+}
+</style>
