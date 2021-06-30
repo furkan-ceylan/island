1d6f5777f36ece883a083ece97e5ab4e72d3c14e
furkan ceylan <ceylan.furkan@outlook.com>
2021-07-01 00:56:17 +0300

    Add TheHeader Component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link>
+    <TheHeader />
+    <!-- <router-link to="/">Home</router-link> -->
   </div>
   <router-view />
 </template>
+
+<script>
+import TheHeader from '@/components/TheHeader'
+
+export default {
+  name: 'App',
+  components: { TheHeader },
+}
+</script>
 
 <style scoped></style>
