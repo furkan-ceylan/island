21e52c58b3c12efc684bfd42b08a3b1e7e3a6c6f
furkan ceylan <ceylan.furkan@outlook.com>
2021-06-30 04:17:40 +0300

    Add post route to index

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const helmet = require('helmet')
 const morgan = require('morgan')
 const userRoute = require('./routes/users')
 const authRoute = require('./routes/auth')
+const postRoute = require('./routes/posts')
 
 dotenv.config()
 
@@ -26,6 +27,7 @@ app.use(morgan('common'))
 
 app.use('/api/users', userRoute)
 app.use('/api/auth', authRoute)
+app.use('/api/posts', postRoute)
 
 app.listen(8800, () => {
   console.log('backend server is running!')
