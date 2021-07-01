67edfbb9fd87ca7b9af40f8175853274b8ab692e
furkan ceylan <ceylan.furkan@outlook.com>
2021-07-01 03:20:03 +0300

    Add left sidebar, right sidebar and container

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,10 +1,49 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <div class="home__left-sidebar">
+      <LeftSidebar />
+    </div>
+    <div class="home__container"></div>
+    <div class="home__right-sidebar">
+      <RightSidebar />
+    </div>
+  </div>
 </template>
 
 <script>
+import LeftSidebar from '@/components/LeftSidebar'
+import RightSidebar from '@/components/RightSidebar'
+
 export default {
   name: 'Home',
-  components: {},
+  components: { LeftSidebar, RightSidebar },
 }
 </script>
+
+<style scoped>
+.home {
+  display: grid;
+  grid-template-columns: 3fr 10fr 3fr;
+  grid-gap: 3.3rem;
+}
+
+.home__container {
+  background-color: white;
+  border-radius: 2rem;
+  position: sticky;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 800px;
+  margin-top: 5rem;
+}
+
+.home__right-sidebar {
+  margin-right: 1rem;
+  margin-top: 5rem;
+}
+
+.home__left-sidebar {
+  margin-top: 5rem;
+}
+</style>
