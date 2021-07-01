1c41513cbdb13155eac7bb43182d3931ee89d61d
furkan ceylan <ceylan.furkan@outlook.com>
2021-07-01 03:20:37 +0300

    Create RightSidebar Component

---
+++ frontend/src/components/RightSidebar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="right-sidebar"></div>
+</template>
+
+<script>
+export default {
+  name: 'RightSidebar',
+}
+</script>
+
+<style scoped>
+.right-sidebar {
+  background-color: var(--light);
+  border-radius: 2rem;
+  position: sticky;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 500px;
+}
+</style>
