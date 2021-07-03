d84c8d599f8f251c3e47e7633c179e2a574d7568
furkan ceylan <ceylan.furkan@outlook.com>
2021-07-03 05:05:04 +0300

    Add post text post and post image post methods, validations, file reader and add post pop ups

--- frontend/src/components/TheHeader.vue
+++ frontend/src/components/TheHeader.vue
@@ -12,7 +12,22 @@
             <input type="text" placeholder="Search" />
             <i class="material-icons">search</i>
           </div>
-          <button class="btn btn-add">Add a Post</button>
+          <button
+            @click="
+              ;(openAddTextPost = !openAddTextPost), (openAddImagePost = false)
+            "
+            class="btn btn-textadd"
+          >
+            Add a Text Post
+          </button>
+          <button
+            @click="
+              ;(openAddImagePost = !openAddImagePost), (openAddTextPost = false)
+            "
+            class="btn btn-imageadd"
+          >
+            Add a Image Post
+          </button>
         </div>
       </div>
       <div class="header__user">
@@ -23,14 +38,146 @@
         />
       </div>
     </div>
+    <div class="add-post" v-if="openAddTextPost">
+      <h2 class="add-post__title">
+        Add a Text Post
+      </h2>
+      <label class="input">
+        <input
+          class="input__field"
+          type="text"
+          placeholder=" "
+          v-model="textTitle"
+        />
+        <span class="input__label">Title</span>
+      </label>
+      <label class="input">
+        <input
+          class="input__field"
+          type="text"
+          placeholder=" "
+          v-model="textDescription"
+        />
+        <span class="input__label">Description</span>
+      </label>
+      <span class="input__label warn" v-if="fillError"
+        >Please fill in all fields</span
+      >
+      <div class="options">
+        <button class="btn-addpost" @click="addTextPost">Add</button>
+        <button @click="openAddTextPost = !openAddTextPost" class="btn-addpost">
+          Close
+        </button>
+      </div>
+    </div>
+    <div class="add-post" v-if="openAddImagePost">
+      <h2 class="add-post__title">
+        Add an Image Post
+      </h2>
+      <label class="input">
+        <input
+          class="input__field"
+          type="text"
+          placeholder=" "
+          v-model="imageTitle"
+        />
+        <span class="input__label">Title</span>
+      </label>
+      <label class="input">
+        <input
+          class="input__field"
+          type="file"
+          accept="image/*"
+          placeholder=" "
+          @change="onFileChange"
+        />
+        <span class="input__label">Image</span>
+      </label>
+      <span class="input__label warn" v-if="fillError"
+        >Please fill in all fields</span
+      >
+      <div class="options">
+        <button class="btn-addpost" @click.prevent="addImagePost()">Add</button>
+        <button
+          @click="openAddImagePost = !openAddImagePost"
+          class="btn-addpost"
+        >
+          Close
+        </button>
+      </div>
+    </div>
   </header>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+
+export default {
+  name: 'TheHeader',
+  data() {
+    return {
+      posts: [],
+      textTitle: '',
+      imageTitle: '',
+      textDescription: '',
+      image: '',
+      isTextPost: false,
+      isImagePost: false,
+      openAddTextPost: false,
+      openAddImagePost: false,
+      fillError: false,
+    }
+  },
+  methods: {
+    async addTextPost() {
+      if (this.textDescription === '' || this.textTitle === '') {
+        this.fillError = true
+      } else {
+        const response = await axios.post('http://localhost:3000/api/posts/', {
+          description: this.textDescription,
+          title: this.textTitle,
+          isTextPost: true,
+          userId: '60df466844d54d0adc94f75e',
+        })
+
+        this.posts.push(response.data)
+        this.textDescription = ''
+        this.textTitle = ''
+        this.openAddTextPost = false
+      }
+    },
+    onFileChange(e) {
+      const files = e.target.files || e.dataTransfer.files
+      if (!files.length) return
+      this.createImage(files[0])
+    },
+    createImage(file) {
+      const image = new Image()
+      const reader = new FileReader()
+
+      reader.onload = (e) => {
+        this.image = e.target.result
+      }
+      reader.readAsDataURL(file)
+    },
+    async addImagePost() {
+      if (this.imageTitle === '' || this.image === '') {
+        this.fillError = true
+      } else {
+        const response = await axios.post('http://localhost:3000/api/posts/', {
+          title: this.imageTitle,
+          isTextPost: true,
+          userId: '60df467d44d54d0adc94f760',
+        })
+        this.posts.push(response.data)
+        this.imageTitle = ''
+      }
+    },
+  },
+}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .header {
   height: 10vh;
   display: flex;
@@ -78,6 +225,7 @@ export default {}
   display: flex;
   align-items: center;
   justify-content: center;
+  position: relative;
 }
 
 .header__main-right-search {
@@ -125,15 +273,141 @@ export default {}
   border-radius: 100%;
 }
 
-.btn-add {
+.btn {
+  margin-left: 1rem;
+}
+
+.btn-textadd {
   transform: translate(0, 3px);
   transition: 0.4s;
 }
 
-.btn-add:hover {
+.btn-textadd:hover {
   background-color: #e64e49;
   transition: 0.4s;
   box-shadow: 0px 15px 15px -5px rgba(0, 0, 0, 0.2);
   transform: translate(0, -3px);
+}
+
+.btn-imageadd {
+  transform: translate(0, 3px);
+  transition: 0.4s;
+  background-color: var(--purple);
+}
+
+.btn-imageadd:hover {
+  background-color: #5863c8;
+  transition: 0.4s;
+  box-shadow: 0px 15px 15px -5px rgba(0, 0, 0, 0.2);
+  transform: translate(0, -3px);
+}
+
+@keyframes slide {
+  from {
+    background-position: 0 0;
+  }
+
+  to {
+    background-position: -120px 60px;
+  }
+}
+
+.add-post {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: auto;
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1.6rem 3rem;
+  border: 3px solid black;
+  border-radius: 5px;
+  background: white;
+  box-shadow: 8px 8px 0 rgba(0, 0, 0, 0.2);
+  z-index: 1;
+  &__title {
+    margin-bottom: 1rem;
+    margin-top: 1rem;
+  }
+}
+
+.description {
+  font-size: 1.1rem;
+  margin-bottom: 1.6rem;
+  margin-top: 0;
+}
+.btn-addpost {
+  color: inherit;
+  font-family: inherit;
+  font-size: inherit;
+  background: white;
+  padding: 0.3rem 3.4rem;
+  border: 3px solid black;
+  margin-right: 2.6rem;
+  box-shadow: 0 0 0 black;
+  transition: all 0.2s;
+}
+
+.btn-addpost:last-child {
+  margin: 0;
+}
+
+.btn-addpost:hover {
+  box-shadow: 0.4rem 0.4rem 0 black;
+  transform: translate(-0.4rem, -0.4rem);
+}
+
+.btn-addpost:active {
+  box-shadow: 0 0 0 black;
+  transform: translate(0, 0);
+}
+
+.options {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.input {
+  position: relative;
+
+  &__label {
+    position: absolute;
+    left: 0;
+    top: 0;
+    padding: calc(var(--size-bezel) * 0.75) calc(var(--size-bezel) * 0.5);
+    margin: calc(var(--size-bezel) * 0.75 + 3px) calc(var(--size-bezel) * 0.5);
+    white-space: nowrap;
+    transform: translate(0, 0);
+    transform-origin: 0 0;
+    transition: transform 120ms ease-in;
+    font-weight: bold;
+    line-height: 1.2;
+  }
+  &__field {
+    box-sizing: border-box;
+    display: block;
+    width: 300px;
+    border: 3px solid currentColor;
+    padding: calc(var(--size-bezel) * 1.5) var(--size-bezel);
+    color: currentColor;
+    background: transparent;
+    border-radius: var(--size-radius);
+    margin-bottom: 1rem;
+
+    &:focus,
+    &:not(:placeholder-shown) {
+      & + .input__label {
+        transform: translate(0.25rem, -90%) scale(0.8);
+        color: var(--pink);
+      }
+    }
+  }
+}
+
+.warn {
+  color: var(--red);
 }
 </style>
