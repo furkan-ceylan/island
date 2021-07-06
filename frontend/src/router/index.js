e77e4d0c060f0d71bf911466879840b5e7a9bdb8
furkan ceylan <ceylan.furkan@outlook.com>
2021-07-06 19:16:57 +0300

    Add routerecordraw

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/auth/Login.vue'
 import Signup from '../views/auth/Signup.vue'
@@ -33,14 +33,6 @@ const routes = [
     component: PostDetail,
     props: true,
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
 ]
 
 const router = createRouter({
