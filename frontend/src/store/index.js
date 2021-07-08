4c0be7d2ec124be75435c5268d8e2561111a32d1
furkan ceylan <ceylan.furkan@outlook.com>
2021-07-08 04:36:22 +0300

    Delete stores

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,12 +1,14 @@
-import { createStore } from 'vuex'
+import { Commit, createStore } from 'vuex'
 
 export default createStore({
   state: {
+    // authenticated: false,
   },
   mutations: {
+    // SET_AUTH: (state, auth) => (state.authenticated = auth),
   },
   actions: {
+    // setAuth: ({ commit }, auth) => commit('SET_AUTH', auth),
   },
-  modules: {
-  }
+  modules: {},
 })
