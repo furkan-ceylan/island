b97f8881e1853b40e5b4397d30b19352db8d3434
furkan ceylan <ceylan.furkan@outlook.com>
2021-07-12 18:56:10 +0300

    Add environments for tokens

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -27,7 +27,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     type: 'OAuth2',
-    user: process.env.AUTH_USER,
+    user: process.env.AUTH_USER_EMAIL,
     clientId: process.env.AUTH_CLIENT_ID,
     clientSecret: process.env.AUTH_CLIENT_SECRET,
     refreshToken: process.env.AUTH_REFRESH_TOKEN,
