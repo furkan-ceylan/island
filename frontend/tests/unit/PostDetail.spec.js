f48e7ddf3c901341ff114695854a237d464b2def
furkan ceylan <ceylan.furkan@outlook.com>
2021-07-28 02:28:46 +0300

    Create PostDetail tests

---
+++ frontend/tests/unit/PostDetail.spec.js
@@ -0,0 +1,32 @@
+import { mount, shallowMount } from '@vue/test-utils'
+import PostDetail from '@/components/PostDetail.vue'
+
+describe('PostDetail', () => {
+  it('should if comment is empty fill the error', async () => {
+    const wrapper = mount(PostDetail)
+    const button = wrapper.find('button')
+    expect(wrapper.vm.commentModel).toBe('')
+    await button.trigger('click')
+    expect(wrapper.vm.fillError).toBe(false)
+  })
+
+  it('should after adding a comment stop the loader', async () => {
+    const wrapper = mount(PostDetail)
+    const button = wrapper.find('button')
+    await button.trigger('click')
+    expect(wrapper.vm.isLoading).toBe(false)
+  })
+
+  it('should add a comment', async () => {
+    const wrapper = mount(PostDetail, {
+        data() {
+          return {
+            commentModel: 'Test Comment'
+          }
+        }
+      })
+    const button = wrapper.find('button')
+    await button.trigger('click')
+    expect(wrapper.vm.commentModel).toBe('Test Comment')
+  })
+})
