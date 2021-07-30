cf760ed36c46e1c4761d530bb9767cbd0c0e2ad2
furkan ceylan <ceylan.furkan@outlook.com>
2021-07-31 01:26:44 +0300

    Add post and fetch post from vuex

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 
 export default createStore({
   state: {
-    user: '',
+    user: [],
+    posts: [],
   },
   getters: {},
   actions: {
@@ -12,6 +13,30 @@ export default createStore({
         headers: { token: localStorage.getItem('token') },
       })
       commit('SET_USER', response.data.user)
+    },
+    async fetchPosts({ commit }) {
+      const responseId = await axios.get('auth/user', {
+        headers: { token: localStorage.getItem('token') },
+      })
+
+      const currentUser = responseId.data.user._id
+
+      const responsePosts = await axios.get('posts/timeline/' + currentUser)
+
+      const posts = responsePosts.data.sort((p1, p2) => {
+        return new Date(p2.createdAt) - new Date(p1.createdAt)
+      })
+
+      commit('SET_POSTS', posts)
+    },
+    async addPost({ commit }, post) {
+      await axios.post('posts/', post)
+      commit('ADD_POST', post)
+    },
+    async addImagePost({ commit }, {post, formData}) {
+      await axios.post('posts/', post)
+      await axios.post('posts/upload', formData)
+      commit('ADD_POST', post)
     },
     async refreshToken() {
       await axios
@@ -28,6 +53,12 @@ export default createStore({
   mutations: {
     SET_USER(state, user) {
       state.user = user
+    },
+    SET_POSTS(state, posts) {
+      state.posts = posts
+    },
+    ADD_POST(state, post) {
+      state.posts.push(post)
     },
   },
 })
